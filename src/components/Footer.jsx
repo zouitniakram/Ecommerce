1cea9c1877f2343529a4fd56284ce18701c189ec
zouitniakram <164836132+zouitniakram@users.noreply.github.com>
2024-12-18 02:22:04 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            
+            <a className="text-dark fs-4" href="https://github.com/zouitniakram" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Main, Product, Footer } from "../components";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Product />
+      
+    </>
+  )
+}
+
+export default Home
